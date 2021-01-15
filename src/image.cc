18f713c9ff6d2bf7a85631f71ec3de475b62ed3e
Daniel Qian <danielqian2015@gmail.com>
2021-01-14 16:11:57 -0800

    Editing contents of cc files

--- src/image.cc
+++ src/image.cc
@@ -1,8 +1,3 @@
-// CS 128 - Naive Bayes
-
-#ifndef _IMAGE_H_
-#define _IMAGE_H_
-
 #include <cstdlib>
 #include <istream>
 #include <fstream>
@@ -12,29 +7,5 @@
 using namespace std;
 
 namespace NaiveBayes {
-  // 28 * 28 pixels is an image
-  constexpr size_t kImageSize = 28;
-
-  class Image {
-
-    public:
-    
-      /**
-      * Constructor for the Image class. Initializes an image containing an
-      * array of pixels representing whether or not a pixel is shaded
-      *
-      * @param img_string a string to be parsed that contains 
-      *   the pixels for the image of size 28 * 28
-      */
-
-      Image(const std::string &img_string);
-
-    /**
-      * Array representing pixels. Values are 0 if the pixel is unshaded, 1
-      * if the pixel is shaded for the corresponding location [][].
-      */
-
-      int pixels[kImageSize][kImageSize];
-  };
+ 
 } 
-#endif
