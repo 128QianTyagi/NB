c8059c9e0e33f615b487675da949f07ef8e41649
parth <parth.27@outlook.com>
2021-01-17 15:35:33 -0600

    Streamlined execution using macros

--- src/driver.cc
+++ src/driver.cc
@@ -4,4 +4,32 @@
 #include "../includes/image.h"
 #include "../includes/model.h"
 
-int main(int argc, char* argv[]) { return 0; }
+#define TRAIN 0
+
+int main(int argc, char* argv[]) {
+	std::ifstream traininglabels_ifstream("data/traininglabels");
+	std::ifstream trainingimages_ifstream("data/trainingimages");
+	std::istream & traininglabels = traininglabels_ifstream;
+	std::istream & trainingimages = trainingimages_ifstream;
+
+	std::ifstream testinglabels_ifstream("data/testlabels");
+	std::ifstream testingimages_ifstream("data/testimages");
+	std::istream & testinglabels = traininglabels_ifstream;
+	std::istream & testingimages = trainingimages_ifstream;
+
+	#if TRAIN == 1
+	NaiveBayes::Model model(traininglabels, trainingimages);
+	std::ofstream out_stream("data/TrainedModelTest_a.txt");
+  	std::ostream & out = out_stream;
+  	out << model;
+
+  	#endif
+  	#if TRAIN == 0
+	std::ifstream inf_stream("data/TrainedModelTest_a.txt");
+  	std::istream & model_s = inf_stream;
+  	NaiveBayes::Classifier classifier(model_s, testingimages);
+  	std::vector<int> vec = classifier.Classify();
+  	std::cout << classifier.Accuracy(vec, testinglabels);
+  	#endif
+	return 0; 
+}
